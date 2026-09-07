fbc415c5d9617f4c5a0877cbda8e1daac94251aa
logact <logact@qq.com>
2026-09-07 12:10:57 +0800

    feat: define basic lifeMap domain

--- src/domain/lifeMap.ts
+++ src/domain/lifeMap.ts
@@ -3,119 +3,119 @@ import { v4 } from "uuid";
 import { Edge } from "./edge";
 import { Node } from "./node";
 
-export class LayerView{
-    map:LifeMap;
-    fowardSteps:number = 0;
-    isolatedNodes:Node[];
-    edges:Edge[];
+export class LayerView {
+    map: LifeMap;
+    forwardSteps: number = 0;
+    isolatedNodes: Node[];
+    edges: Edge[];
 
-    
-    constructor(map:LifeMap){
+
+    constructor(map: LifeMap) {
         this.map = map;
         this.edges = this.map.rootEdges;
-        this.isolatedNodes =this.map.rootNodes;
+        this.isolatedNodes = this.map.rootNodes;
     }
-    refresh(layer:number){
+    refresh(layer: number) {
+        this.forwardSteps = layer
         this.edges = this.map.rootEdges;
-        this.isolatedNodes =this.map.rootNodes;
-        while(layer>0){
+        this.isolatedNodes = this.map.rootNodes;
+        while (layer > 0) {
             this.nextLayer()
-            layer--   
+            layer--
         }
-        this.fowardSteps = layer
+
     }
 
-    prevLayer(){
-        this.edges = this.edges.filter(e => e.layer < this.fowardSteps)
-        this.fowardSteps--;
+    prevLayer() {
+        if (this.forwardSteps <= 0) { return }
+
+        this.edges = this.edges.filter(e => e.layer < this.forwardSteps)
+        this.forwardSteps--;
     }
     // show more detail of the current edges and also keep the current edges.
     /**
      * 
      * @returns return true when not reach the bottom, return false when reach to the bottom.
      */
-    nextLayer():boolean{
+    nextLayer(): boolean {
         const newEdges: Edge[] = this.edges.flatMap(e =>
             e.childrenEdges.length ? e.childrenEdges : []
         );
-        if(newEdges.length === 0){
+        if (newEdges.length === 0) {
             return false
-        }else{
+        } else {
             this.edges = this.edges.concat(newEdges);
-            this.fowardSteps++
+            this.forwardSteps++
         }
         return true
     }
 
-   
+
 }
-export class LifeMap{
-    rootNodes:Node[]
-    rootEdges:Edge[]
+export class LifeMap {
+    rootNodes: Node[]
+    rootEdges: Edge[]
 
 
-    constructor(nodes: Node[]=[],edges: Edge[]=[]){
+    constructor(nodes: Node[] = [], edges: Edge[] = []) {
         this.rootNodes = nodes
         this.rootEdges = edges
     }
-    save(){
+    save() {
         //TODO persist db/cloud
     }
-    load(){
+    load() {
         //TODO load from db/cloud
     }
-    removeNode(node:Node){
+    removeNode(node: Node) {
         // removeEdge re-adds an isolated node to rootNodes, so the node
         // itself must be spliced out only after its edges are gone
         const edges = [...node.startEdges, ...node.endEdges];
-        edges.forEach(e=>this.removeEdge(e))
+        edges.forEach(e => this.removeEdge(e))
         const index = this.rootNodes.indexOf(node);
         if (index !== -1) {
             this.rootNodes.splice(index, 1);
         }
     }
 
-    updateEdgeLayer(rootEdge:Edge , change:number){
-        rootEdge.layer  = rootEdge.layer + change
+    updateEdgeLayer(rootEdge: Edge, change: number) {
+        rootEdge.layer = rootEdge.layer + change
         const children = rootEdge.childrenEdges
-        if(!children || children.length <= 0){
+        if (!children || children.length <= 0) {
             return
         }
 
-        for(let childEdge of children){
-            this.updateEdgeLayer(childEdge,change)
+        for (let childEdge of children) {
+            this.updateEdgeLayer(childEdge, change)
         }
 
 
     }
 
-    
+
     // remove the edge and save its children edge
     // TODO add the paramter deletedRecrusivly to support delete recursivly 
-    removeEdge(edge:Edge){
+    removeEdge(edge: Edge) {
         const index = this.rootEdges.indexOf(edge);
         if (index !== -1) {
             this.rootEdges.splice(index, 1);
         }
-        if(edge.parentEdge){
-            edge.parentEdge.childrenEdges = edge.parentEdge.childrenEdges.filter(e=>e.id !== edge.id)
+        if (edge.parentEdge) {
+            edge.parentEdge.childrenEdges = edge.parentEdge.childrenEdges.filter(e => e.id !== edge.id)
         }
 
         const childrenEdges = edge.childrenEdges
-        if(childrenEdges && childrenEdges.length > 0){
-            childrenEdges.forEach(e=>{
-                if(edge.parentEdge){
-                    edge.parentEdge.childrenEdges.push(e)
-                    e.parentEdge = edge.parentEdge
-                }else{
-                    e.parentEdge = undefined
-                    e.layer = 0
-                    this.rootEdges.push(e)
-                }
-                this.updateEdgeLayer(e,-1)
+        childrenEdges.forEach(e => {
+            if (edge.parentEdge) {
+                edge.parentEdge.childrenEdges.push(e)
+                e.parentEdge = edge.parentEdge
+            } else {
+                e.parentEdge = undefined
+                this.rootEdges.push(e)
+            }
+            this.updateEdgeLayer(e, -1)
 
-            })
-        }
+        })
 
 
 
@@ -123,28 +123,29 @@ export class LifeMap{
         const node1 = edge.node1
         const node2 = edge.node2
         const startIndex = node1.startEdges.indexOf(edge)
-        if(startIndex !== -1){
-            node1.startEdges.splice(startIndex,1)
-        } 
+        if (startIndex !== -1) {
+            node1.startEdges.splice(startIndex, 1)
+        }
         const endIndex = node2.endEdges.indexOf(edge)
-        if(endIndex !== -1){
-            node2.endEdges.splice(endIndex,1)
+        if (endIndex !== -1) {
+            node2.endEdges.splice(endIndex, 1)
         }
 
         // add the new isolated node cause by the edge delection to the rootNodes.
-        if(node1.startEdges.length <=0 && node1.endEdges.length <=0 && !this.rootNodes.includes(node1)){
+        if (node1.startEdges.length <= 0 && node1.endEdges.length <= 0 && !this.rootNodes.includes(node1)) {
             this.rootNodes.push(node1)
         }
-        if(node2.startEdges.length <=0 && node2.endEdges.length <=0 && !this.rootNodes.includes(node2)){
+        if (node2.startEdges.length <= 0 && node2.endEdges.length <= 0 && !this.rootNodes.includes(node2)) {
             this.rootNodes.push(node2)
         }
+        edge.childrenEdges = []
     }
 
-    addNode(node:Node):Node{
+    addNode(node: Node): Node {
         // dupling
-    
-        for(let n of this.rootNodes){
-            if(n.id === node.id){
+
+        for (let n of this.rootNodes) {
+            if (n.id === node.id) {
                 return n
             }
         }
@@ -152,11 +153,11 @@ export class LifeMap{
         this.rootNodes.push(node)
         return node
     }
-    addEdge(node1:Node,node2:Node,parentEdge?:Edge):Edge{
-        const edge = new Edge(node1,node2,parentEdge,[])
-        if(!parentEdge){
+    addEdge(node1: Node, node2: Node, parentEdge?: Edge): Edge {
+        const edge = new Edge(node1, node2, parentEdge, [])
+        if (!parentEdge) {
             this.rootEdges.push(edge)
-        }else{
+        } else {
             parentEdge.childrenEdges.push(edge)
             edge.layer = parentEdge.layer + 1
         }
@@ -167,20 +168,20 @@ export class LifeMap{
         return edge
     }
 
-    private removeRootNodeIfPresent(node:Node){
+    private removeRootNodeIfPresent(node: Node) {
         const index = this.rootNodes.indexOf(node)
-        if(index !== -1){
-            this.rootNodes.splice(index,1)
+        if (index !== -1) {
+            this.rootNodes.splice(index, 1)
         }
     }
 
     // expand a edge with only 2 points,generate 2 new edge whose parent is the edge
-    expand(edge:Edge){
-        if(edge.childrenEdges.length > 0){
+    expand(edge: Edge) {
+        if (edge.childrenEdges.length > 0) {
             return
         }
         const newTitle = `${edge.node1.title}-${edge.node2.title}`
-        const newSubNode:Node = {
+        const newSubNode: Node = {
             x: 0,
             y: 0,
             id: v4(),
@@ -188,8 +189,8 @@ export class LifeMap{
             startEdges: [],
             endEdges: []
         }
-        this.addEdge(edge.node1,newSubNode,edge)
-        this.addEdge(newSubNode,edge.node2,edge)
+        this.addEdge(edge.node1, newSubNode, edge)
+        this.addEdge(newSubNode, edge.node2, edge)
 
     }
 
@@ -201,44 +202,44 @@ export class LifeMap{
      * @param edges the edges to be summarzied must belongs to a same parent
      * @returns 
      */
-    summarize(node1:Node,node2:Node,edges:Edge[]):Edge | null{
-        if(edges.length === 0){
+    summarize(node1: Node, node2: Node, edges: Edge[]): Edge | null {
+        if (edges.length === 0) {
             return null
         }
-        
+
         const parentEdge = edges[0].parentEdge
         const parentEdgeId = parentEdge ? parentEdge.id : ""
-        edges.forEach(e=>{
+        edges.forEach(e => {
             let eParentEdgeId = e.parentEdge ? e.parentEdge.id : ""
-            if(parentEdgeId !== eParentEdgeId){
+            if (parentEdgeId !== eParentEdgeId) {
                 throw new Error("Edges to be summarized should belong to the same parent")
-            }  
+            }
         })
-        const summarizedIds = new Set<string>(edges.map(e=>e.id))
+        const summarizedIds = new Set<string>(edges.map(e => e.id))
 
         // detach the summarized edges from their old container
-        if(parentEdge){
-            parentEdge.childrenEdges = parentEdge.childrenEdges.filter(e=>!summarizedIds.has(e.id))
-        }else{
-            this.rootEdges = this.rootEdges.filter(e=>!summarizedIds.has(e.id))
+        if (parentEdge) {
+            parentEdge.childrenEdges = parentEdge.childrenEdges.filter(e => !summarizedIds.has(e.id))
+        } else {
+            this.rootEdges = this.rootEdges.filter(e => !summarizedIds.has(e.id))
         }
 
-        const newSummarizedEdge = new Edge(node1,node2,parentEdge,[])
+        const newSummarizedEdge = new Edge(node1, node2, parentEdge, [])
 
-        if(parentEdge){
+        if (parentEdge) {
             newSummarizedEdge.layer = parentEdge.layer + 1
         }
-        edges.forEach(edge=>{
+        edges.forEach(edge => {
             edge.parentEdge = newSummarizedEdge
 
             newSummarizedEdge.childrenEdges.push(edge)
-            this.updateEdgeLayer(edge,1)
-            
+            this.updateEdgeLayer(edge, 1)
+
         })
 
-        if(parentEdge){
+        if (parentEdge) {
             parentEdge.childrenEdges.push(newSummarizedEdge)
-        }else{
+        } else {
             this.rootEdges.push(newSummarizedEdge)
         }
         return newSummarizedEdge
